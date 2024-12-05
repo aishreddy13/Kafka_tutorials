c6b5f0e4ccda22b0e3359b8aa42afa88152055f5
aishreddy13 <ashreddy91@gmail.com>
2024-12-05 06:08:47 -0500

    kafka-basics
    Initial commit of the project

---
+++ kafka-beginners/kafka-basics/src/main/java/io/condu/demos/kafka/ProducerDemo.java
@@ -0,0 +1,20 @@
+package io.condu.demos.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("hello");
+        //create producer properties
+
+        //create producer
+
+        //send data
+
+        //flush and close the producer
+    }
+}
